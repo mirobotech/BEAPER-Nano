fb6ad5887054963e252912d1e37a4e8f8fdca57c
John Rampelt <johnrampelt@gmail.com>
2025-03-10 21:58:46 -0400

    Update BEAPER-Nano-IO-Test.py

--- MicroPython/BEAPER-Nano-IO-Test/BEAPER-Nano-IO-Test.py
+++ MicroPython/BEAPER-Nano-IO-Test/BEAPER-Nano-IO-Test.py
@@ -1,21 +1,21 @@
 """
 BEAPER Nano I/O Test project
-January 15, 2025
+March 10, 2025
 
 Functional test of all on-board BEAPER Nano I/O devices.
 
-This program includes a SONAR range function designed to test an
-optional 3.3V HC-SR04P ultrasonic distance sensor module plugged
-into headers H1-H4, as well as servo position functions to control
-two servos connected to H5 and H6 from potentiometers RV1 and RV2.
+This program includes a SONAR range function designed to test an optional 3.3V
+HC-SR04P ultrasonic distance sensor module plugged into headers H1-H4, as well
+as servo position functions to control two servos connected to H5 and H6 from
+potentiometers RV1 and RV2.
 
-The program displays the SONAR range and analog input values from
-the ambient light sensor, temperature sensor and both on-board
-potentiometers. Set the jumpers to select the environmental inputs
-(labelled Enviro.) on the BEAPER Nano PCB.
+The program displays the SONAR range and analog input values from the ambient
+light sensor, temperature sensor and both on-board potentiometers. Set the
+jumpers to select the environmental inputs (labelled Enviro.) on the BEAPER
+Nano PCB.
 
-See the https://mirobo.tech/beaper webpage for additional BEAPER
-Nano starter progams and beginner programming activities.
+See the https://mirobo.tech/beaper webpage for additional BEAPER Nano starter
+progams and beginner programming activities.
 """
 
 # Import machine and time functions
@@ -57,9 +57,10 @@ H3IN = ECHO = Pin(14 , Pin.IN, Pin.PULL_UP)
 # H4OUT = H6OUT = Pin(12 , Pin.OUT)
 SERVO2 = PWM(Pin(12), freq=50, duty_u16=4916)
 
-# Tone functions. The tone() function creates a tone using a PWM output. The
-# tone plays until stopped using the noTone() function, or for the duration of
-# time specified (optional, in seconds, other operations are blockd for duration). 
+# Tone functions. The tone() function creates sound at the specficied frequency
+# using PWM output. The tone plays until stopped using the noTone() function, or
+# for the optional specified duration (in seconds). Playing a tone blocks using
+# duration blocks other operations for the specified duration.
 def tone(frequency, duration=None):
     BEEPER.freq(frequency)
     BEEPER.duty_u16(32768)
@@ -68,18 +69,20 @@ def tone(frequency, duration=None):
         noTone()
 
 # Stops the tone. Specifying an optional duration pauses for the duration of
-# time specified (in seconds, other operations for duration).
+# time specified (in seconds, and blocks other operations for the duration).
 def noTone(duration=None):
     BEEPER.duty_u16(0)
     if duration is not None:
         time.sleep(duration)
 
-# Map function. Map value in input range to output range.
+# Map function. Maps a value within the input range to the output range.
 def map(value, in_min, in_max, out_min, out_max):
     return int((value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
-# Servo position functions. Creates servo pulses my mapping degree to PWM pulsewidth.
-# Un-comment appropriate duty_u16 line to map input values between 0-180 degrees to pulse length
+# Servo position functions. Creates continuous servo pulses using PWM. Maps input
+# degree values to PWM puslewidths. Un-comment the appropriate duty_u16 duty cycle
+# to map input the required input range (0-90 or 0-180 degrees) to the output pulse
+# length appopriate for your type of servo.
 def servo1_position(deg):
     SERVO1.duty_u16(map(deg, 0, 90, 3277, 6554))  # 1-2ms pulses for a 90 deg. servo
     # SERVO1.duty_u16(map(deg, 0, 180, 3277, 6554))  # 1-2ms pulses for 180 deg. input to a 90 deg. servo
@@ -92,41 +95,46 @@ def servo2_position(deg):
     # SERVO2.duty_u16(map(deg, 0, 180, 1782, 8192))  # 544us-2500us pulses for a 180 deg. servo
     # SERVO2.duty_u16(map(deg, 0, 180, 1638, 8192))  # 500us-2500us pulses for a 180 deg. servo
 
-# SONAR range function with maximum range limit and error checking.
+# SONAR range function with maximum range limit and ECHO pin error checking.
 # Returns the range of the closest target in cm. The 'max' parameter limits
-# the search range to objects no farther than 'max' (cm). The function returns
-# -1  if previous ranging operation is still in progress, 0 if no target is
-# found within the specified max range, or the range of the closest target in cm.
+# the search range to objects no farther than 'max' cm. The function returns
+# -1 if previous a ranging operation is still in progress, 0 if no target is
+# found within the specified max range, or returns the range to the closest
+# target in cm.
 # Example application:
 # range_cm = sonar_range(100)   # Find closest target within 100 cm
 def sonar_range(max):
     # Check if previous ECHO has finished
     if ECHO.value() == 1:
-        return -1   # ECHO in progress, can't trigger until 10ms after ECHO ends
+        return -1   # ECHO in progress (wait 10ms after ECHO ends before re-triggering)
+    
     # Create TRIG pulse
     TRIG.value(1)
     time.sleep_us(10)
     TRIG.value(0)
-    # HC-SR04P (3.3V-capable module, also labelled as RCWL-9610A 2022) delays for 
-    # 2320us after TRIG pulse and before ECHO starts. Uncomment the lines below to
-    # enable max measurement range of less than 40cm
-    # pulse_duration = time_pulse_us(ECHO, 0, 2500)
-    # if pulse_duration < 0:
-    #     return pulse_duration
     
-    # Wait for ECHO and time ECHO pulse. Set time-out to max range (min. 40 cm)
-    pulse_duration = time_pulse_us(ECHO, 1, (max + 1) * 58)
-    if pulse_duration < 0:
-        return 0                        # Out of range - return 0
-    else:
-        return pulse_duration / 58    # Return range in cm 
+    # Wait for ECHO to go high. HC-SR04P (3.3V-capable modules, also labelled as
+    # RCWL-9610A 2022) delay for approximately 2300us after TRIG pulse ends
+    # before ECHO pulse starts.
+    duration = time_pulse_us(ECHO, 0, 2500)
+    if duration < 0:
+        return duration # ECHO didn't start. Return error code.
+    
+    # Time ECHO pulse. Set time-out value to max range.
+    duration = time_pulse_us(ECHO, 1, (max + 1) * 58)
+    if duration < 0:
+        return 0    # Return 0 if distance is > max range
+    
+    # Return range to target in cm
+    return duration / 58
 
-# LED on
+# Arduino Nano EPS32 LED on
 LED_BUILTIN.value(1)
 
 # Start-up sound
 tone(1000,0.1)
 
+# Brief instructions
 print("Starting BEAPER Nano")
 print("SW2 - LED sequence and tones")
 print("SW3 - light Arduino Nano ESP32 red LED")
@@ -134,8 +142,9 @@ print("SW4 - light Arduino Nano ESP32 green LED")
 print("SW5 - light Arduino Nano ESP32 blue LED")
 time.sleep(1)
 
+# Set timers
 servo_timer = time.ticks_ms()
-sonar_timer = time.ticks_ms()
+sensor_timer = time.ticks_ms()
 
 while True:
     # Chase LEDs
@@ -181,7 +190,9 @@ while True:
     else:
         LED_BLUE.value(1)
     
+    # Update servo inputs every 20ms
     if time.ticks_diff(time.ticks_ms(), servo_timer) > 20:
+        # Reset servo_timer
         servo_timer = time.ticks_ms()
         # Update servos
         rv1_pos = RV1.read_u16()
@@ -191,8 +202,10 @@ while True:
         rv2_angle = map(rv2_pos, 0, 65535, 0, 90)
         servo2_position(rv2_angle)
 
-    if time.ticks_diff(time.ticks_ms(), sonar_timer) > 500:
-        sonar_timer = time.ticks_ms()
+    # Update SONAR range and analog input every 500ms
+    if time.ticks_diff(time.ticks_ms(), sensor_timer) > 500:
+        # Reset sensor_timer
+        sensor_timer = time.ticks_ms()
         # Get and print SONAR range
         range_cm = sonar_range(100)
         print("Range:", int(range_cm), "cm")
@@ -204,4 +217,3 @@ while True:
         print("RV1 position: ", rv1_pos)
         print("RV2 position: ", rv2_pos)
         print("")
-
